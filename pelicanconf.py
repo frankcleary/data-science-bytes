68c680e1a109e8926d3f702cecc1899f9bcf86f5
Frank Cleary <frank@frankcleary.com>
2014-11-01 15:33:54 -0700

    undid conf refactor

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-from sharedconf import *
 
+AUTHOR = u'Frank Cleary'
+SITENAME = u'Data Science Bytes'
 SITEURL = 'http://www.datasciencebytes.com'
-THEME = 'notmyidea'
+THEME = 'themes/notmyidea-cms'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# URL/Save as schemes
+ARTICLE_URL = 'bytes/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'bytes/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+YEAR_ARCHIVE_URL = 'bytes/{date:%Y}/'
+YEAR_ARCHIVE_SAVE_AS = 'bytes/{date:%Y}/index.html'
+MONTH_ARCHIVE_URL = 'bytes/{date:%Y}/{date:%m}/'
+MONTH_ARCHIVE_SAVE_AS = 'bytes/{date:%Y}/{date:%m}/index.html'
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -12,8 +30,16 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Recommended Books', '/recommended-books'),
-         ('Recommended Videos', '/recommended-videos'),)
+LINKS = (('Recommended Books', SITEURL + '/recommended-books'),
+         ('Recommended Videos', SITEURL + '/recommended-videos'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/DSBytes'),)
+
+DEFAULT_PAGINATION = 20
+
+# Twitter button
+TWITTER_USERNAME = 'DSBytes'
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
--- productionconf.py
+++ productionconf.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-from sharedconf import *
 
+AUTHOR = u'Frank Cleary'
+SITENAME = u'Data Science Bytes'
 SITEURL = 'http://www.datasciencebytes.com'
 THEME = 'themes/notmyidea-cms'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# URL/Save as schemes
+ARTICLE_URL = 'bytes/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'bytes/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+YEAR_ARCHIVE_URL = 'bytes/{date:%Y}/'
+YEAR_ARCHIVE_SAVE_AS = 'bytes/{date:%Y}/index.html'
+MONTH_ARCHIVE_URL = 'bytes/{date:%Y}/{date:%m}/'
+MONTH_ARCHIVE_SAVE_AS = 'bytes/{date:%Y}/{date:%m}/index.html'
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,6 +40,9 @@ DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = False
+
+# Twitter button
+TWITTER_USERNAME = 'DSBytes'
 
 DISQUS_SITENAME = 'datasciencebytes' 
 
