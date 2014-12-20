5d5398c4be32e3e55b06d17277707e26690548f5
Frank Cleary <fcleary@berkeley.edu>
2014-12-19 23:49:30 -0800

    include ipynb plugin in prod conf

--- productionconf.py
+++ productionconf.py
@@ -17,8 +17,9 @@ EXTRA_PATH_METADATA = {
     'extra/favicon2.png': {'path': 'favicon.ico'},
 }
 
-PLUGIN_PATHS = ["similar_posts"]
-PLUGINS = ['similar_posts',]
+MARKUP = ('md', 'ipynb')
+PLUGIN_PATHS = ["similar_posts", "./plugins"]
+PLUGINS = ['similar_posts', 'ipynb']
 MAX_RELATED_POSTS = 5
 
 TIMEZONE = 'America/Los_Angeles'
