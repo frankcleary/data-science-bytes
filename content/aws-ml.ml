fec037a4fe7838e1aed1c7325fb11bd4e299c548
Frank Cleary <frank@frankcleary.com>
2015-04-19 21:26:38 -0700

    base post

---
+++ content/aws-ml.ml
@@ -0,0 +1,12 @@
+Title: First Look at AWS Machine Learning
+Date: 2015-04-12
+Category: Tutorials
+Tags: data, AWS, machine learning
+
+Amazon Web Services recently announced [Amazon Machine Learning](http://aws.amazon.com/machine-learning/), promising to make large scale machine learning more accessible to non-experts. I was curious to try out this service so I fed it some weather data from Oakland International Airport to see how well and how easily it could predict the maximum temperature on a day given the weather during the last 7 days.
+
+[Data Format](http://docs.aws.amazon.com/machine-learning/latest/dg/creating_datasources.html)
+
+[Data Documentation](http://www1.ncdc.noaa.gov/pub/data/cdo/documentation/GHCND_documentation.pdf)
+
+[More Date Documentation](http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt)
